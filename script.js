7a76afbaf94a23de173f57a811c65f58e4cb4e45
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-01-26 22:31:58 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+
+
+
+//Possible variables and values
+let chooseCritera = true;
+lowerCase = "abcdefghijklmnopqrstuvxyz";lowerCase.split(" "),
+upperCase = "ABCDEFGHIKLMNOPQRSTVXYZ";upperCase.split(" "),
+numbers = [0,1,2,3,4,5,6,7,8,9],
+specialChar = "! # $ % & ( ) * + - / : ; < = > ? @ \ ^ _ | ";specialChar.split(" "),
+randomIndex = Math.floor(Math.random() * lowerCase.length),
+password = "";
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", passwordText);
+
+//Generate random password-funcion
+function writePassword() {
+  randomIndex = Math.floor(Math.random() * lowerCase.length);
+}
+
+
+var uppcerCase = confirm("Include Upper Case letters");
+//run the random letter generator function
+if (upperCase) {function name(params) {
+  
+}
+}
+
+//user is prompted to choose the criteria for the password
+if (chooseCritera === true) {
+  chooseCritera = prompt("Include Lower Case letters");
+  
+}
+
+if (upperCase === true) {
+  
+} else {
+  
+}
+
+var lowerCase = confirm("Include Lower Case letters");
+//run the random letter generator function
+
+var numbers = confirm("Include numbers");
+//run the random number generator function
+
+var specialChar = confirm("Include Special Characters")
+//run the random specialChar generator function
+
+
+
+
+for (let i = 0; i < 8; i++) {
+  password = letters[i];
+  Math.floor(Math.random() * letters.length)
+  
+}
+
